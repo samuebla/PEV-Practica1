8835adbe363ef791736b3b8fba7010459ecbe2e9
Joseda <jrave@ucm.es>
2022-02-16 15:43:58 +0100

    Iniciada la plantilla de AG

--- G12P1/src/main.java
+++ G12P1/src/main.java
@@ -1,13 +1,26 @@
 
 public class main {
 
-	public main() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ey");
+	}
+	
+	public void createInterface() {
+//		// define your data
+//		double[] generaciones = { 1, 2, 3, 4, 5, 6, 7, 8, 9 ,10 };
+//		double[] fitness = { 12, 25, 32, 45, 65, 67 , 70, 72, 73, 76};
+//		// create your PlotPanel (you can use it as a JPanel)
+//		Plot2DPanel plot = new Plot2DPanel();
+//		// define the legend position
+//		plot.addLegend("SOUTH");
+//		// add a line plot to the PlotPanel
+//		plot.addLinePlot("EVOLUCIÓN", generaciones, fitness);
+//		// put the PlotPanel in a JFrame like a JPanel
+//		JFrame frame = new JFrame("a plot panel");
+//		frame.setSize(600, 600);
+//		frame.setContentPane(plot);
+//		frame.setVisible(true);
 	}
 
 }
