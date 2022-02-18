d4921957ea0d5cbcdcfd2de65cca93fba8a057ce
Joseda <jrave@ucm.es>
2022-02-18 16:59:59 +0100

    añadido Main

--- Practica1_PEv/src/graphGUI.java
+++ Practica1_PEv/src/graphGUI.java
@@ -20,48 +20,11 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.widgets.Combo;
 
-import org.math.plot.*;
-
 public class graphGUI extends Composite {
 	private Text text;
 	private Text text_1;
 	private Text text_2;
-	
-	public static void main(String[] args) {
-		Display display = new Display();
-		Shell shell = new Shell(display);
-		shell.setLayout(new GridLayout(1, false));
-
-		graphGUI ayMisChiles = new graphGUI(shell,SWT.NONE);
-
-		shell.pack();
-		shell.open();
-		while (!shell.isDisposed()) {
-			if (!display.readAndDispatch())
-				display.sleep();
-		}
-		display.dispose();
-	}
-	
-	public static void createInterface() {
-		// define your data
-		double[] generaciones = { 1, 2, 3, 4, 5, 6, 7, 8, 9 ,10 };
-		double[] fitness = { 12, 25, 32, 45, 65, 67 , 70, 72, 73, 76};
 		
-		System.out.println("Hoa");
-		// create your PlotPanel (you can use it as a JPanel)
-//		Plot2DPanel plot = new Plot2DPanel();
-//		// define the legend position
-//		plot.addLegend("SOUTH");
-//		// add a line plot to the PlotPanel
-//		plot.addLinePlot("EVOLUCIÓN", generaciones, fitness);
-		// put the PlotPanel in a JFrame like a JPanel
-		JFrame frame = new JFrame("a plot panel");
-		frame.setSize(600, 600);
-//		frame.setContentPane(plot);
-		frame.setVisible(true);
-	}
-//	
 	/**
 	 * Create the composite.
 	 * @param parent
