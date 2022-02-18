0e33ec6bc480a619fb64e262455a198298fe91d3
Joseda <jrave@ucm.es>
2022-02-18 16:44:50 +0100

    Cambiado gitignore

--- Practica1_PEv/src/graphGUI.java
+++ Practica1_PEv/src/graphGUI.java
@@ -19,6 +19,7 @@ import org.eclipse.swt.widgets.DateTime;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.widgets.Combo;
+
 import org.math.plot.*;
 
 public class graphGUI extends Composite {
@@ -46,19 +47,21 @@ public class graphGUI extends Composite {
 		// define your data
 		double[] generaciones = { 1, 2, 3, 4, 5, 6, 7, 8, 9 ,10 };
 		double[] fitness = { 12, 25, 32, 45, 65, 67 , 70, 72, 73, 76};
+		
+		System.out.println("Hoa");
 		// create your PlotPanel (you can use it as a JPanel)
-		Plot2DPanel plot = new Plot2DPanel();
-		// define the legend position
-		plot.addLegend("SOUTH");
-		// add a line plot to the PlotPanel
-		plot.addLinePlot("EVOLUCIÓN", generaciones, fitness);
+//		Plot2DPanel plot = new Plot2DPanel();
+//		// define the legend position
+//		plot.addLegend("SOUTH");
+//		// add a line plot to the PlotPanel
+//		plot.addLinePlot("EVOLUCIÓN", generaciones, fitness);
 		// put the PlotPanel in a JFrame like a JPanel
 		JFrame frame = new JFrame("a plot panel");
 		frame.setSize(600, 600);
-		frame.setContentPane(plot);
+//		frame.setContentPane(plot);
 		frame.setVisible(true);
 	}
-	
+//	
 	/**
 	 * Create the composite.
 	 * @param parent
