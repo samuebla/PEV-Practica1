1a23c9c729dd7b27c5a2ca12291c7024a6e68c6f
Joseda <jrave@ucm.es>
2022-03-08 20:24:56 +0100

    Creada la Population

---
+++ P1/src/individual/Population.java
@@ -0,0 +1,31 @@
+package individual;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Population {
+
+	private List<Chromosome> population_;
+	
+	public Population() {
+		// TODO Auto-generated constructor stub
+		population_ = new ArrayList<Chromosome>();
+	}
+	
+	public Population(List<Chromosome> pob){
+		population_ = new ArrayList();
+		for(Chromosome ind : pob)
+			population_.add(new Chromosome(ind));
+	}
+	
+	public List<Chromosome> getPoblacion() {
+		return population_;
+	}
+	
+	public String toString(){
+		String cadena = "";
+		for (Chromosome in : population_)
+			cadena += in.getFenotipo() + "\n";
+
+		return cadena;
+	}
+}
